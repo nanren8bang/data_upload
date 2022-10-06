47ad8b0db548caee0ec60fbbb9483036c95c211f
nanren8bang <69335701+nanren8bang@users.noreply.github.com>
2022-10-06 17:05:17 -0400

    Update variant_drugDA.py

--- da/mysql/variant_drugDA.py
+++ da/mysql/variant_drugDA.py
@@ -1,46 +1,13 @@
 ##File          : variant_drugDA.py 
-##Date Created  : 05/05/2022, 19:48:22
+##Date Created  : 04/05/2022, 19:48:22
 ##Author        : Ken Liu 
 
-from bo.mysql.variant_drug import variant_drug as BO 
+ 
 from da.mysql.connectionFactory import connectionFactory as CF 
 
 
 class variant_drugDA:
-    ##1: C
-    @staticmethod
-    def runInsertListOfTuple(sql, val):
-        CF.runInsertListOfTuple(sql, val)
-
-    ##2: R
-
-
-    @staticmethod
-    def getAll():
-        myBOList=[]
-        mySQL="""
-            select *  from variant_drug
-        """
-        results=CF.runSelect(mySQL)
-        for result in results:
-            myBOList.append(BO.fromDict(result))
-        return myBOList
-        
-
-    @staticmethod
-    def getOne():
-        pass
-        
-
-    ##3: U
-
-
-    ##4: D
-
-
-    ##5: Misc
-
-
+    
     @staticmethod
     def runSQL(mySQL):
         CF.runSQL(mySQL)
